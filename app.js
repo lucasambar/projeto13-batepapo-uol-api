de66285162592c379b274d606de534653aa4f988
Lucas <lucass.ambar@gmail.com>
2022-11-10 18:56:56 -0300

    post route participants

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import { MongoClient } from "mongodb";
+import daysjs from "dayjs"
 
 //CONFIGURAÇÕES
 dotenv.config();
@@ -18,5 +19,29 @@ try {
 } catch (err) {console.log(err)}
 
 const collectionUsers = mongoClient.db("uol").collection("users")
+const collectionMessages = mongoClient.db("uol").collection("messages")
+
+
+//post participants
+app.post("/participants", async (req,res) => {
+    const {name} = req.body
+
+    if (!name) {res.status(422).send("'Name' deve ser strings, não vazio!")}
+
+    const user = {name,lastStatus: Date.now()}
+    try {
+        await collectionUsers.insertOne(user)
+    } catch (erro) {console.log(erro)} 
+
+
+    const message = {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:MM:SS'}
+    try {
+        await collectionMessages.insertOne(message)
+        res.sendStatus(201)
+    } catch (erro) {console.log(erro)}
+})
+
+
+
 
 app.listen(process.env.PORT, () => console.log(`Server running in port: ${process.env.PORT}`))
